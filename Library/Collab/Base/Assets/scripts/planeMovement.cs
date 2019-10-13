bb86606639434628fa326390fb0ee1de634a16af
Gurneet <sbgurneet@gmail.com>
2019-10-13 07:24:47 +0200

    first commit

---
+++ Library/Collab/Base/Assets/scripts/planeMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class planeMovement : NetworkBehaviour {
+
+    public Rigidbody2D rb;
+    
+
+
+    public float speed;
+
+    float MovementY;
+
+    private float Y_pos;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (hasAuthority== false )
+        {
+            return;
+    }
+        MovementY = Input.GetAxisRaw("Vertical") * (speed / 10);
+        scrollScript.speed = 1f ;
+        coins.speed = 5f;
+        Y_pos = Mathf.Clamp(rb.transform.position.y, -5, 5);
+        rb.transform.position =new Vector3(rb.transform.position.x,Y_pos, rb.transform.position.z);
+    }
+
+    public void FixedUpdate()
+    {
+        Move(MovementY);
+    }
+    public void Move(float i)
+    {
+        Vector2 displacement = new Vector2(rb.position.x, rb.position.y + i);
+        rb.transform.position = displacement;
+
+    }
+}
---
+++ Library/Collab/Base/Assets/scripts/spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public float countdown = 3f;
+
+    public GameObject go;
+
+    public Transform[] CoinSpawns;
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0)
+        {
+            Spawn();
+            countdown = 3f;
+
+        }
+    }
+
+     public void Spawn()
+    {
+        Quaternion rotation = CoinSpawns[Random.Range(0,CoinSpawns.Length)].transform.rotation;
+        GameObject coin = Instantiate(go, CoinSpawns[Random.Range(0,CoinSpawns.Length)].position, rotation);
+        Destroy(coin, 10f);
+
+    }
+}
